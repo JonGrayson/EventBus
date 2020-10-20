5214cc7d4676f464a36c4e11659931f7010338f0
JonGrayson <49501559+JonGrayson@users.noreply.github.com>
2020-10-19 17:03:07 -0700

    Add files via upload

---
+++ Scripts/EventPublisher.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class EventPublisher : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            EventBus.TriggerEvent("Shoot");
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            EventBus.TriggerEvent("Launch");
+        }
+
+        if(Input.GetKeyDown("e"))
+        {
+            EventBus.TriggerEvent("Motto");
+        }
+
+        if(Input.GetKeyDown("p"))
+        {
+            EventBus.TriggerEvent("Play");
+        }
+
+        if(Input.GetKeyDown("s"))
+        {
+            EventBus.TriggerEvent("Stop");
+        }
+
+        if(Input.GetKeyDown("r"))
+        {
+            EventBus.TriggerEvent("LetsGetAtEr");
+        }
+    }
+}
